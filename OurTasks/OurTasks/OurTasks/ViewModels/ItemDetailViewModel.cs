bdf8c4de37c120f675090fed0617db9464d873ca
Robert <rgreen2005@msn.com>
2017-10-04 20:37:23 -0700

    Items and details pages working
    Home on 10/4/17

--- OurTasks/OurTasks/OurTasks/ViewModels/ItemDetailViewModel.cs
+++ OurTasks/OurTasks/OurTasks/ViewModels/ItemDetailViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OurTasks
+﻿namespace OurTasks
 {
     public class ItemDetailViewModel : BaseViewModel
     {
